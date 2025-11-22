4d22002474737e89c3e5d4b0105b8b40d732ce84
Алексей Шашин <alexshashin.2023@gmail.com>
2025-11-22 17:52:28 +0500

    реализованный класс заказчика Шашин

---
+++ Classes/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingGifts_Шашин.Classes
+{
+    public class Customer
+    {
+        public string fio_user {  get; set; }
+        public string message_text { get; set; }
+        public string adress {  get; set; }
+        public DateTime date_Send_Message { get; set;  }
+        public string email { get; set; }
+    }
+}
